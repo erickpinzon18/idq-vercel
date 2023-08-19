ea805ae560fe71503e8f5343864683459a41f6aa
SrCosmicCat <gatitosplus000@gmail.com>
2023-08-19 12:42:34 -0600

    config change

--- config/config.js
+++ config/config.js
@@ -1,8 +1,17 @@
 // Validate if the environment is development or production
-if (process.env.NODE_ENV !== 'production') {
-    const dotenv = import('dotenv');
-    dotenv.config();
-}
+
+// export function config() {
+//     if (process.env.NODE_ENV !== 'production') {
+//         // Import dotenv if the environment is development with es6
+//         import('dotenv').then((dotenv) => {
+//             dotenv.config();
+//         });
+//     }
+// }
+
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const MONGO_CONNECTION_LINK = process.env.MONGO_CONNECTION_LINK; 
-export const FILE_SERVER = process.env.FILE_SERVER; 
+export const FILE_SERVER = process.env.FILE_SERVER;
+export const PORT = process.env.PORT;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import Express from "express";
 import { connectMongoDB } from "../utils/connection.js";
 import { router as userRouter } from "../routes/user.js";
 import { router as authRouter } from "../routes/auth.js";
+import { PORT } from "../config/config.js";
 
 connectMongoDB();
 
 const app = Express();
-const PORT = 3000;
 
 app.disable("x-powered-by");
 app.use(Express.json());
