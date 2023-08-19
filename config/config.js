d205015a1250d861c7dc6ed46ca3128dc74a8997
SrCosmicCat <gatitosplus000@gmail.com>
2023-08-19 11:55:28 -0600

    add new scripts

--- config/config.js
+++ config/config.js
@@ -1,2 +1,8 @@
+// Validate if the environment is development or production
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = import('dotenv');
+    dotenv.config();
+}
+
 export const MONGO_CONNECTION_LINK = process.env.MONGO_CONNECTION_LINK; 
 export const FILE_SERVER = process.env.FILE_SERVER; 
